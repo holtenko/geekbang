d436bbf9d0578d195de8fe70b0922dfb40e7ed2f
holten <holten.ko@gmail.com>
2021-02-16 18:03:04 +0800

    递归方式二分查找

--- algo/src/search/BinarySearch.java
+++ algo/src/search/BinarySearch.java
@@ -8,7 +8,9 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int targetIndex = binarySearch(nums, 5);
+        int binarySearchRecursive = binarySearchRecursive(nums, 5);
         System.out.println(targetIndex);
+        System.out.println(binarySearchRecursive);
     }
 
     public static int binarySearch(int[] nums, int target) {
@@ -29,5 +31,27 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    public static int binarySearchRecursive(int[] nums, int target) {
+        int length = nums.length;
+        if (length < 1) {
+            return -1;
+        }
+        return binarySearchRecursive(nums, target, 0, length - 1);
+    }
+
+    public static int binarySearchRecursive(int[] nums, int target, int low, int high) {
+        if (low > high) {
+            return -1;
+        }
+        int mid = low + ((high - low) >> 1);
+        if (target == nums[mid]) {
+            return mid;
+        } else if (target > nums[mid]) {
+            return binarySearchRecursive(nums, target, mid + 1, high);
+        } else {
+            return binarySearchRecursive(nums, target, low, mid - 1);
+        }
     }
 }
